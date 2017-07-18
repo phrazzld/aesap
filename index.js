6239d55340dee2fb3b354679b1dd60e1400d382d
jacogrande <jacogrande@Jacos-Machine.local>
2017-07-18 14:16:31 -0700

    updated index

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
+// modules
 var express = require("express")
 var app = express()
 var sanitizer = require("express-sanitizer")
 var bodyParser = require("body-parser")
 var config = require("./config")
 
+// middleware 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(sanitizer())
 
+
+// functions
+
+var colors=["#EB4D5C","#007AB8","#000","#4D394B","#FAD529","#298FC3"];
+
+function sendGif(pretext, image_url, text){
+  pretext = pretext || "";
+  text = text || "";
+  var color = colors[Math.floor(Math.random()*colors.length)];
+
+  return {
+    data:{
+      slack:{
+        attachments:[
+          {
+            fallback:"gif gif gif",
+            color:color,
+            pretext:pretext,
+            image_url:image_url,
+            text:text
+          }
+        ]
+      }
+    }
+  }
+
+}
+
+
+// router
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Methods", "GET")
@@ -26,31 +58,7 @@ app.post("/handler", function (req, res) {
 
   if(intent === "Gif"){
     console.log("issa jif?");
-    response={
-      data:{
-        slack:{
-          attachments:[
-            {
-              fallback:"gif gif gif",
-              color:"#007AB8",
-              pretext:"doh!",
-              image_url:"http://media3.giphy.com/media/kEKcOWl8RMLde/giphy.gif"
-            }
-          ]
-        }
-      }
-      // speech:"testing"
-      // data:{
-      //   slack:{
-      //     attachments:{
-      //       fallback:"gif gif gif",
-      //       color:"#007AB8",
-      //       pretext:"doh!",
-      //       image_url:"http://media3.giphy.com/media/kEKcOWl8RMLde/giphy.gif"
-      //     }
-      //   }
-      // }
-    }
+    response=sendGif("doh!","http://media3.giphy.com/media/kEKcOWl8RMLde/giphy.gif");
   }
   else{
     response={speech:"yo!"}
