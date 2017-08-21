2a0df0c453922d252b5a25d4a85bbc0db3997486
jacogrande <jacogrande@Jacos-Machine.local>
2017-08-21 10:46:12 -0700

    Updated Config

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   apiKey: process.env.ASAP_API_KEY,
   apiUser: process.env.ASAP_API_USER,
   apiPw: process.env.ASAP_API_PASSWORD,
-  apiOrgId: process.env.ASAP_API_ORGID
+  apiOrgId: process.env.ASAP_API_ORGID,
   slackVerToken: process.env.SLACK_VERIFICATION_TOKEN,
   slackCliSecret: process.env.SLACK_CLIENT_SECRET,
   slackCliId: process.env.SLACK_CLIENT_ID
